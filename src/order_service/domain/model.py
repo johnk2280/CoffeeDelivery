a8f32746ac7f31fed75e961e8bf954cb241f0486
Evgeny Kungurov <eakungur@mts.ru>
2024-05-19 21:28:45 +0500

    ch07 [add] Order domain model

--- src/order_service/domain/model.py
+++ src/order_service/domain/model.py
@@ -1,5 +1,15 @@
 import uuid
 from dataclasses import dataclass
+from enum import Enum
+
+
+class StatusChoice(str, Enum):
+    CREATED = 'created'
+    PAID = 'paid'
+    PROGRESS = 'progress'
+    CANCELLED = 'cancelled'
+    DISPATCHED = 'dispatched'
+    DELIVERED = 'delivered'
 
 
 @dataclass(frozen=True)
@@ -9,3 +19,10 @@ class OrderItem:
     product: str
     size: int
     quantity: int
+
+
+@dataclass(frozen=True)
+class Order:
+    id: uuid.UUID
+    items: list[OrderItem]
+    status: StatusChoice
