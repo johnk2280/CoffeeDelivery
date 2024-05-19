1f9c46d221ecc87d02467048eb82b6e8e3a5146a
Evgeny Kungurov <eakungur@mts.ru>
2024-05-19 21:57:03 +0500

    ch07 [upd]

---
+++ src/order_service/domain/__init__.py
@@ -0,0 +1,9 @@
+from .model import Order
+from .model import OrderItem
+from .model import StatusChoice
+
+__all__ = [
+    'StatusChoice',
+    'OrderItem',
+    'Order',
+]
---
+++ src/order_service/service_layer/services.py
@@ -0,0 +1,7 @@
+from typing import Any
+
+from domain import Order
+
+
+def place_order(items: list[dict[str, Any]]) -> list[Order]:
+    pass
