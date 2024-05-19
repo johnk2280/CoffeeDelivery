3e0fc567f1616769ed1edd6a88ae35cbe4ba2506
Evgeny Kungurov <eakungur@mts.ru>
2024-05-19 22:07:44 +0500

    ch07 [add] test_place_order

---
+++ src/order_service/service_layer/__init__.py
@@ -0,0 +1,5 @@
+from .services import place_order
+
+__all__ = [
+    'place_order',
+]
---
+++ src/tests/order_service/service_layer/unit/test_place_order.py
@@ -0,0 +1,18 @@
+from typing import Any
+
+import pytest
+
+from domain import Order
+from service_layer import place_order
+
+
+@pytest.mark.parametrize('items, expected, expectation', [
+    ([{}], None, pytest.raises(Exception)),
+])
+def test_place_order(
+    items: list[dict[str, Any]],
+    expected: Order,
+    expectation: Any,
+) -> None:
+    with expectation:
+        assert place_order(items) == expected
