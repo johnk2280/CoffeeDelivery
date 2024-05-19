bf95f90d3b7d5862ca19d037ea5508f68f2fe4a7
Evgeny Kungurov <johnk2280@gmail.com>
2024-05-19 23:01:22 +0500

    ch07 [upd] place_order

--- src/order_service/service_layer/services.py
+++ src/order_service/service_layer/services.py
@@ -6,6 +6,7 @@ from domain import OrderItem
 
 
 def place_order(items: list[dict[str, Any]]) -> Order:
+
     return Order(
         id=uuid.uuid4(),
         items=[OrderItem(**el) for el in items],
