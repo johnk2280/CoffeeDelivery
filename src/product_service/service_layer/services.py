6429ab9cf614d12c8786acd230fd7a689af442bb
Evgeny Kungurov <johnk2280@gmail.com>
2024-06-13 22:33:58 +0500

    [upd] test_add_product

--- src/product_service/service_layer/services.py
+++ src/product_service/service_layer/services.py
@@ -1,10 +1,12 @@
 import decimal
 from collections.abc import Sequence
 
+from domain.models import Currency
 from domain.models import Ingredient
 from domain.models import Product
 from domain.models import ProductType
 from domain.models import Supplier
+from domain.models import Unit
 
 
 def add_product(
@@ -13,5 +15,9 @@ def add_product(
     product_type: ProductType,
     name: str,
     price: decimal.Decimal,
+    currency: Currency,
+    quantity: int,
+    available: bool,
+    unit: Unit,
 ) -> Product:
     pass
--- src/tests/product_service/service_layer/unit/test_add_product.py
+++ src/tests/product_service/service_layer/unit/test_add_product.py
@@ -2,6 +2,7 @@ import datetime
 import decimal
 import uuid
 
+from domain.models import Currency
 from domain.models import Ingredient
 from domain.models import Product
 from domain.models import ProductType
@@ -13,28 +14,37 @@ from service_layer import add_product
 def test_add_product():
     unit = Unit(id=uuid.uuid4(), name='шт')
     type_ = ProductType(id=uuid.uuid4(), name='выпечка')
-    ingredient_1 = Ingredient(id=uuid.uuid4(), name='мука пшеничная')
-    ingredient_2 = Ingredient(id=uuid.uuid4(), name='вода')
-    ingredient_3 = Ingredient(id=uuid.uuid4(), name='дрожжи хлебопекарные')
+    ingredient_1 = Ingredient(
+        id=uuid.uuid4(),
+        name='мука пшеничная',
+        products=None
+    )
+    ingredient_2 = Ingredient(id=uuid.uuid4(), name='вода', products=None)
+    ingredient_3 = Ingredient(
+        id=uuid.uuid4(),
+        name='дрожжи хлебопекарные',
+        products=None
+    )
     supplier = Supplier(
         id=uuid.uuid4(),
         name='ООО Бодрые единороги',
         address='Москва',
         inn=777,
     )
-    product = Product(
-        id=uuid.uuid4(),
-        name='хлеб пшеничный',
-        price=decimal.Decimal('50'),
-        available=True,
-        type=type_,
-        ingredients={ingredient_1, ingredient_2, ingredient_3},
-        supplier=supplier,
-        quantity=10,
-        unit=unit,
-        created_at=datetime.datetime.now(datetime.UTC),
-        updated_at=datetime.datetime.now(datetime.UTC),
-    )
+    currency = Currency(id=uuid.uuid4(), name='Руб')
+    # product = Product(
+    #     id=uuid.uuid4(),
+    #     name='хлеб пшеничный',
+    #     price=decimal.Decimal('50'),
+    #     available=True,
+    #     type=type_,
+    #     ingredients={ingredient_1, ingredient_2, ingredient_3},
+    #     supplier=supplier,
+    #     quantity=10,
+    #     unit=unit,
+    #     created_at=datetime.datetime.now(datetime.UTC),
+    #     updated_at=datetime.datetime.now(datetime.UTC),
+    # )
 
     result = add_product(
         supplier=supplier,
@@ -42,4 +52,10 @@ def test_add_product():
         product_type=type_,
         name='хлеб пшеничный',
         price=decimal.Decimal('50'),
+        currency=currency,
+        quantity=10,
+        available=True,
+        unit=unit,
     )
+
+    assert result.name == 'хлеб пшеничный'
