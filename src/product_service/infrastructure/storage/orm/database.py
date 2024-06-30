be4378a9bd01cc94a4eb849aef88b9c00d39bc2c
Evgeny Kungurov <johnk2280@gmail.com>
2024-06-30 20:56:04 +0500

    ch07 [upd] database.py

--- src/product_service/infrastructure/storage/orm/database.py
+++ src/product_service/infrastructure/storage/orm/database.py
@@ -1,8 +1,20 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 
+from config import get_settings
+
+settings = get_settings()
+
 DATABASE_PARAMS = dict(
-    pool_size=10,
-    max_overflow=10,
+    pool_size=settings.DB_POOL_SIZE,
+    max_overflow=settings.DB_MAX_OVERFLOW,
 )
 
-engine = create_async_engine(**DATABASE_PARAMS)
+engine = create_async_engine(settings.database_url, **DATABASE_PARAMS)
+async_session_maker = async_sessionmaker[AsyncSession](
+    engine,
+    autoflush=False,
+    autocommit=False,
+    expire_on_commit=False,
+)
